69c7f612fbd962d26a1e6b7be1aa87fae6b7cde9
Victor Santiago <vrsa9208@gmail.com>
2019-03-08 12:51:52 -0600

    Retrieve a JSON with javalin

--- src/main/java/com/vrsa9208/frameworks/javalin/HelloWorld.java
+++ src/main/java/com/vrsa9208/frameworks/javalin/HelloWorld.java
@@ -7,6 +7,35 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		Javalin app = Javalin.create().start(7000);
 		app.get("/", ctx -> ctx.result("Hello World"));
+
+		app.get("/response", ctx -> ctx.json(new Response("OK", "A message")));
+	}
+
+}
+
+class Response {
+	private String status;
+	private String message;
+
+	public Response(String status, String message) {
+		this.status = status;
+		this.message = message;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
 	}
 
 }
