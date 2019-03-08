e413474adc8d66b33af7a4e89beca14aa2a7bc15
vvo0002 <vvo0002@mail.wal-mart.com>
2019-03-08 12:45:31 -0600

    Javalin Hello World

---
+++ src/main/java/com/vrsa9208/frameworks/javalin/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.vrsa9208.frameworks.javalin;
+
+import io.javalin.Javalin;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		Javalin app = Javalin.create().start(7000);
+		app.get("/", ctx -> ctx.result("Hello World"));
+	}
+
+}
