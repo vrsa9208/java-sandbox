5ba26e43e1c42abeadc9f1b64d9fce33632adbdc
Victor Santiago <vrsa9208@gmail.com>
2019-03-29 14:29:53 -0600

    Method references demo

--- src/main/java/com/vrsa9208/tests/method_references/MethodReferencesDemo.java
+++ src/main/java/com/vrsa9208/tests/method_references/MethodReferencesDemo.java
@@ -2,6 +2,7 @@ package com.vrsa9208.tests.method_references;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 public class MethodReferencesDemo {
@@ -17,5 +18,14 @@ public class MethodReferencesDemo {
 		System.out.println(fruitSupplier.get("Apple").get().getDescription());
 		System.out.println(fruitSupplier.get("Orange").get().getDescription());
 		System.out.println(fruitSupplier.get("Pineapple").get().getDescription());
+		
+		Consumer<Fruit> fruitConsumer = MethodReferencesDemo::prepare;
+		
+		fruitConsumer.accept(fruitSupplier.get("Apple").get());
+		fruitConsumer.accept(fruitSupplier.get("Pineapple").get());
+	}
+	
+	public static void prepare(Fruit fruit) {
+		System.out.println("Preparing a delicious " + fruit.getName() + " cocktail");
 	}
 }
